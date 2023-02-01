dc28886232078dae2bc73f24e866b6dbb45f0e39
OlesyaAsanova88 <asa88.88@bk.ru>
2023-02-01 17:42:46 +0300

    first quiz

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const {quiz} = defineProps(['quiz'])
+</script>
+
+<template>
+       <div class="card">
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions </p>
+        </div>
+      </div>
+</template>
+
+<style scoped>
+    .card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+    margin-right: 20px;
+  }
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0;
+  }
+  .card-text h2 {
+    font-weight: 700;
+  }
+</style>
